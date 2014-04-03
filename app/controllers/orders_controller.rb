72c928852b67260f222ab497e730e96a5c01edd8
Rheeno <rheeno-ryan@hotmail.com>
2014-04-03 14:36:11 -0500

    Shipping

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -46,12 +46,10 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-		
         format.html { redirect_to store_url, notice: 
           'Thank you for your order.' }
         format.json { render action: 'show', status: :created,
           location: @order }
-		  
       else
         format.html { render action: 'new' }
         format.json { render json: @order.errors,
